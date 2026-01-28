177465b99c16da6e865a54d3b2cf60eb4aa52ddc
iby7 <60022209+iby7@users.noreply.github.com>
2026-01-28 19:58:51 +0100

    finished C02 ex00

--- projects/C02/ex00/ft_strcpy.c
+++ projects/C02/ex00/ft_strcpy.c
@@ -3,43 +3,40 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ibtunc <ibtunc@student.42.fr>              +#+  +:+       +#+        */
+/*   By: ibtunc <ibtunc@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/28 13:43:24 by ibtunc            #+#    #+#             */
-/*   Updated: 2026/01/28 15:19:42 by ibtunc           ###   ########.fr       */
+/*   Updated: 2026/01/28 19:58:23 by ibtunc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
+/*#include <unistd.h>
+#include <stdio.h>*/
 
 char	*ft_strcpy(char *dest, char *src)
 {
-	int i;
 	int k;
 
-	i = 0;
 	k = 0;
-	while (*src++)
-	{
-		i++;
-	}
-	src = src - i;
-	while (k < i)
+	while (src[k] != '\0')
 	{
 		dest[k] = src[k];
-		write(1, &src[k], 1);
+		printf("%c\n", dest[k]);
 		k++;
 	}
+	dest[k] = '\0';
 	return (dest);
-}
-int main(void)
-{	
-	// char* dest;
-	// char* src;
-	// char t1[4] = "test";
-	// char t2[4] = "abcd";
 	
-	// dest = &t1;
-	// src = &t2;
-	ft_strcpy("test", "abcd");
 }
+
+/*int main(void)
+{	
+	char* dest;
+	char* src;
+	char t1[4] = "test";
+	char t2[4] = "abcd";
+	
+	dest = t1;
+	src = t2;
+	ft_strcpy(t1, t2);
+}*/
