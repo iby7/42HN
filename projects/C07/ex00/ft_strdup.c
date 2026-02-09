f3a9a533148e5d1572ce7db040a13fb31f691e78
Ibrahim Tunc <ibtunc@1-B-26.42heilbronn.de>
2026-02-09 11:55:56 +0100

    formated norminette

--- projects/C07/ex00/ft_strdup.c
+++ projects/C07/ex00/ft_strdup.c
@@ -3,42 +3,43 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strdup.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ibtunc <ibtunc@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*   By: ibtunc <ibtunc@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/08 23:27:33 by ibtunc            #+#    #+#             */
-/*   Updated: 2026/02/08 23:41:27 by ibtunc           ###   ########.fr       */
+/*   Updated: 2026/02/09 11:55:13 by ibtunc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include <unistd.h>
 
-char *ft_strdup(char *src)
+char	*ft_strdup(char *src)
 {
-	char* str;
-	int i;
+	char	*str;
+	int		i;
 
 	i = 0;
-	while(src[i])
+	while (src[i])
 		i++;
-	str = (char*)malloc(i);
+	str = (char *)malloc(i);
 	i = 0;
-	while(src[i])
+	while (src[i])
 	{
 		str[i] = src[i];
 		i++;
 	}
-	return  str;
+	return (str);
 }
 
-int main()
-{	
-	char *str = ft_strdup("test");
-	
-	while(*str)
+int	main(void)
+{
+	char	*str;
+
+	str = ft_strdup("test");
+	while (*str)
 	{
 		write(1, str, 1);
 		str++;
 	}
-	return 0;
+	return (0);
 }
--- projects/C07/ex01/ft_range.c
+++ projects/C07/ex01/ft_range.c
@@ -3,44 +3,44 @@
 /*                                                        :::      ::::::::   */
 /*   ft_range.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ibtunc <ibtunc@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*   By: ibtunc <ibtunc@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/08 23:42:17 by ibtunc            #+#    #+#             */
-/*   Updated: 2026/02/09 00:29:49 by ibtunc           ###   ########.fr       */
+/*   Updated: 2026/02/09 11:55:37 by ibtunc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <stdio.h>
 
-int *ft_range(int min, int max)
+int	*ft_range(int min, int max)
 {
-	int *arr;
-	int i;
+	int	*arr;
+	int	i;
 
 	i = 0;
-	if(min >= max)
-		return NULL;
+	if (min >= max)
+		return (NULL);
 	arr = malloc((max - min) * 4);
-	while(max - min > 0)
+	while (max - min > 0)
 	{
 		arr[i] = min;
 		min++;
 		i++;
 	}
-	return arr;
+	return (arr);
 }
 
-int main()
+int	main(void)
 {
-	int *arr = ft_range(3, 6);
-	//int i = 0;
+	int	*arr;
 
-	while(*arr)
+	arr = ft_range(3, 6);
+	while (*arr)
 	{
 		printf("%d", *arr);
 		arr++;
 	}
-	return 0;
+	return (0);
 }
