ae5397c306809d57c4a8859547b3e1046295aa20
iby7 <60022209+iby7@users.noreply.github.com>
2026-02-10 21:45:18 +0100

    fixed C06 ex03

--- projects/C06/ex03/ft_sort_params.c
+++ projects/C06/ex03/ft_sort_params.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_sort_params.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ibtunc <ibtunc@student.42.fr>              +#+  +:+       +#+        */
+/*   By: ibtunc <ibtunc@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/09 12:31:23 by ibtunc            #+#    #+#             */
-/*   Updated: 2026/02/10 16:45:28 by ibtunc           ###   ########.fr       */
+/*   Updated: 2026/02/10 21:44:56 by ibtunc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,33 +22,35 @@ int	ft_strcmp(char *s1, char *s2)
 	return (*s1 - *s2);
 }
 
-int	main(int argc, char *argv[])
+void	bubble_sort(int argc, char **argv)
 {
 	int		i;
-	int		len;
 	int		j;
 	char	*temp;
 
-	(void)argc;
-	len = 1;
-	j = 0;
-	while (argv[len])
-		len++;
-	while (j < len)
+	j = 1;
+	while (j < argc)
 	{
 		i = 1;
-		while (i < len - 1 - j)
+		while (i < argc - j)
 		{
 			if (ft_strcmp(argv[i], argv[i + 1]) > 0)
 			{
 				temp = argv[i + 1];
 				argv[i + 1] = argv[i];
 				argv[i] = temp;
-				i++;
 			}
+			i++;
 		}
 		j++;
 	}
+}
+
+void	print_params(char **argv)
+{
+	int i;
+	char* temp;
+	
 	i = 1;
 	while (argv[i])
 	{
@@ -61,5 +63,12 @@ int	main(int argc, char *argv[])
 		write(1, "\n", 1);
 		i++;
 	}
+}
+
+int	main(int argc, char *argv[])
+{
+	
+	bubble_sort(argc, argv);
+	print_params(argv);
 	return (0);
 }
